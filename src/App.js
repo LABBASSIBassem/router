a98218f258740162b3c32d837844df72b4cfda74
unknown <bassem.labbassi@gmail.com>
2019-12-21 16:44:12 +0100

    firt-commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , { Component }from 'react';
 import './App.css';
+import Home from './component/Home';
+import About from './component/About'; 
+import Contact from './component/Contact';
+import NavBar from './component/NavBar';
+import {BrowserRouter, Route} from 'react-router-dom';
 
-function App() {
+export default class App extends Component{
+
+
+  render()
+  {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <NavBar />
+      <Route exact path='/' component={Home} />
+      <Route path='/about' component={About} />
+      <Route path='/Contact' component={Contact} />
+      </BrowserRouter>
     </div>
   );
+  }
 }
-
-export default App;
